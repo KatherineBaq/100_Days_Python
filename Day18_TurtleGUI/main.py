121a2577aaa4ad7d91fb99ccde38e48316794677
Katherine Baquero <kabaquerod@unal.edu.co>
2022-01-25 00:58:14 +0100

    Making a spirograph

--- Day18_TurtleGUI/main.py
+++ Day18_TurtleGUI/main.py
@@ -4,7 +4,7 @@ from turtle import Turtle, Screen
 tim = Turtle()
 tim.shape("turtle")
 tim.color("red")
-
+tim.speed("fastest")
 
 # ## Draw a square
 # for _ in range(4):
@@ -18,18 +18,41 @@ tim.color("red")
 #     tim.forward(10)
 #     tim.pendown()
 
-## Draw shapes
-for i in range(3,30):
-
-    angle = 360 / i
+def random_colors():
     #create a random color
     r = random.random()
     g = random.random()
     b = random.random()
-    tim.pencolor((r,g,b))
-    for _ in range(i):
-        tim.forward(50)
-        tim.right(angle)
+    return (r,g,b)
+
+## Draw shapes
+# for i in range(3,30):
+#     angle = 360 / i
+#     tim.pencolor(random_colors())
+#     for _ in range(i):
+#         tim.forward(50)
+#         tim.right(angle)
+
+
+# # Draw a random walk
+# directions=[0,90,180,270]
+
+# tim.pensize(15)
+# for _ in range(200):
+#     tim.pencolor(random_colors())
+#     #tim.forward(random.randint(1,5)*10)
+#     tim.forward(30)
+#     tim.setheading(random.choice(directions))
+#     #tim.pensize(random.randint(1,20))
+
+## Draw a spirograph
+num_circles = 100
+angle = 360/num_circles
+for _ in range(num_circles):
+    tim.circle(100)
+    tim.left(angle)
+    tim.pencolor(random_colors())
+
 
 
 screen = Screen()
