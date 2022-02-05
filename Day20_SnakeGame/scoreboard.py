c0d289bd1474176d1c181c954007108a6b5050db
Katherine Baquero <kabaquerod@unal.edu.co>
2022-02-05 22:32:27 +0100

    Fixed the name of the project from day 24 to day 23

--- Day20_SnakeGame/scoreboard.py
+++ Day20_SnakeGame/scoreboard.py
@@ -6,24 +6,26 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.hideturtle()
         self.penup()
-        self.color("white")
-        self.write_screen()
-
-    def write_screen(self):
-        self.clear()
         self.goto(0, 270)
-        self.write(f"Score: {self.score}", False, align=ALIGNMENT, font= FONT)
+        self.color("white")
+        self.update_scoreboard()
 
-        #self.write(f"Score: ", False, align="Center")
-        #self.write((-300,0),True)
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", False, align=ALIGNMENT, font= FONT)
+
 
     def increase_score(self):
         self.score += 1
-        self.write_screen()
+        self.update_scoreboard()
 
-    def game_over(self):
-        self.goto(0,0)
-        self.write("GAME OVER!", False, align=ALIGNMENT, font= FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_scoreboard()
+
 
