13b55e7adb23e8b03e0afd098e655a3e64fd1fe2
Katherine Baquero <kabaquerod@unal.edu.co>
2022-02-12 00:24:41 +0100

    Day 24 automatic letter writing with different names on each one.

--- day24-Files/main.py
+++ day24-Files/main.py
@@ -6,3 +6,26 @@
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+path_starting_letter = "Input/Letters/starting_letter.txt"
+path_names = "Input/Names/invited_names.txt"
+path_send_letters = "Output/ReadyToSend/"
+
+
+with open(path_names) as file:
+    names = file.readlines()
+
+for name in names:
+    with open(path_starting_letter) as file_sl:
+        contents = file_sl.read()
+        name_clean = name.strip('\n')
+        new_contents = contents.replace("[name]", name_clean)
+        print(new_contents)
+        with open(f"{path_send_letters}letter_for_{name_clean}", mode="w") as file_send:
+            file_send.write(new_contents)
+
+# with open("my_file.txt", mode="a") as file:
+#     file.write("\nNew Text")
+#
+# with open("my_new_file.txt", mode="w") as file:
+#     file.write("New file!!")
