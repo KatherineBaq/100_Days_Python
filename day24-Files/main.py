8f4395a5f4fe3c908603accd33e339d4f98423b0
Katherine Baquero <kabaquerod@unal.edu.co>
2022-02-13 00:45:04 +0100

    Reorganized comments and look

--- day24-Files/main.py
+++ day24-Files/main.py
@@ -1,15 +1,14 @@
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
-    
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+# for each name in invited_names.txt
+# Replace the [name] placeholder with the actual name.
+# Save the letters in the folder "ReadyToSend".
+
+# Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
+# Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
+# Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
 
 path_starting_letter = "Input/Letters/starting_letter.txt"
 path_names = "Input/Names/invited_names.txt"
 path_send_letters = "Output/ReadyToSend/"
-
 
 with open(path_names) as file:
     names = file.readlines()
