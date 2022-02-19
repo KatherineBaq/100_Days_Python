c66a23649c6307de794d1cf49058be81393dbf4b
Katherine Baquero <kabaquerod@unal.edu.co>
2022-02-19 21:18:25 +0100

    Added function to generate password and fixed small details

--- Day29-PasswMang_tkinter/main.py
+++ Day29-PasswMang_tkinter/main.py
@@ -1,8 +1,27 @@
-from tkinter import * #Import all classes and constants
-from tkinter import messagebox #Import a module
+from tkinter import *  # Import all classes and constants
+from tkinter import messagebox  # Import a module
+import random
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    passw_letter = random.choices(letters, k=random.randint(8, 10))
+    passw_symb = random.choices(symbols, k=random.randint(2, 4))
+    passw_numb = random.choices(numbers, k=random.randint(2, 4))
+    password_list = []
+    password_list = passw_letter + passw_numb + passw_symb
+    random.shuffle(password_list)
+    password = ''.join(password_list)
+    input_password.delete(0, END)
+    input_password.insert(0, password)
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
@@ -10,11 +29,11 @@ def save():
     website = input_website.get()
     email = input_email.get()
     password = input_password.get()
-    if len(website) == 0 | len(email) == 0 | len(password) == 0:
+    if len(website) == 0 or len(email) == 0 or len(password) == 0:
         messagebox.showwarning(title="Oops", message="Please don't leave any fields empty!")
     else:
         is_ok = messagebox.askyesno(title=website, message=f"These are the details entered: \n"
-                                               f"Email: {email}\n Password: {password}")
+                                                           f"Email: {email}\n Password: {password}")
         if is_ok:
             with open("data.txt", "a") as file:
                 file.write(f"{website} | {email} | {password}\n")
@@ -45,19 +64,19 @@ label_password = Label(text="Password:")
 label_password.grid(column=0, row=3)
 
 # Entry
-input_website = Entry(width=35)
+input_website = Entry(width=43)
 input_website.grid(column=1, row=1, columnspan=2)  # Columnspan indicates that takes the space of two columns
 input_website.focus()  # To indicate where the cursor should start
 
-input_email = Entry(width=35)
+input_email = Entry(width=43)
 input_email.grid(column=1, row=2, columnspan=2)  # Columnspan indicates that takes the space of two columns
 input_email.insert(0, "kata@mail.com")
 
-input_password = Entry(width=21)
+input_password = Entry(width=25)
 input_password.grid(column=1, row=3)
 
 # Buttons
-button_generate = Button(text="Generate Password", command="")
+button_generate = Button(text="Generate Password", command=generate_password)
 button_generate.grid(column=2, row=3)
 
 button_add = Button(text="Add", width=36, command=save)
